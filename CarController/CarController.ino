5c616cbfb26e9a4d8d66040641afe89d827ebb4e
Philip Ekholm <philipekholm@protonmail.com>
2017-04-13 14:22:23 +0200

    Added serial communication to car

---
+++ CarController/CarController.ino
@@ -0,0 +1,95 @@
+/*
+* CarController 
+* 
+* By Philip E, Henrik F
+* 2017-04-05 12:05
+* 
+* Program for stopping motor in the event of obstacle
+* and allowing steering
+*/
+
+#include <Servo.h>
+
+Servo pointer;
+const byte servoPin = 8; 
+
+/* Motor Driver */
+const byte dir1PinA = 5;  //Backwards pin
+const byte dir2PinA = 3; //Forwards pin
+const byte speedPinA = 11; // Needs to be a PWM pin to be able to control motor speed
+
+void setup(){
+   Serial.begin(9600); 
+    
+   //Define L298N Dual H-Bridge Motor Controller Pins
+
+   pinMode(dir1PinA,OUTPUT);
+   pinMode(dir2PinA,OUTPUT);
+   pinMode(speedPinA,OUTPUT);
+   pointer.attach(servoPin);
+
+   motor_run(0, 1, 10);
+   steer(50, 10);
+}
+
+void loop(){
+    String command = "";
+  
+    // send data only when you receive data:
+    if (Serial.available() > 0) {
+        // read the incoming byte:
+        command = Serial.readString();
+        send_command(command, 50);
+    }
+}
+
+/*
+*   The golden method! Control the motor with
+*   a certain speed (0 to 100). Direction: 0 reverse, 1 forward.
+*/
+
+void motor_run(int speed, int direction, int time){
+    if(direction == 1){
+        analogWrite(speedPinA, speed);//Sets speed variable via PWM 
+        digitalWrite(dir1PinA, LOW);
+        digitalWrite(dir2PinA, HIGH);
+    }
+    else if (direction == 0) {
+        analogWrite(speedPinA, speed);
+        digitalWrite(dir1PinA, HIGH);
+        digitalWrite(dir2PinA, LOW);
+    }
+
+    delay(time);
+}
+
+void steer(int capacity, int time){
+    if(capacity > 100 || capacity < 0){
+       capacity = 50;
+    }
+    
+    int adjustment = 10.24 * capacity;
+    
+    //Steering between 75 degrees and 95 degrees from car right.
+    int pos = map(adjustment, 0, 1024, 70, 95);
+    pointer.write(pos);
+
+    delay(time);
+}
+
+void send_command(String command, int time){
+    if(command.charAt(0) == '1'){
+        motor_run(command.substring(1).toInt(), 1, time);
+    }
+    else if(command.charAt(0) == '2'){
+        steer(command.substring(1).toInt(), time);
+    }
+    else if(command.charAt(0) == '3'){
+        motor_run(command.substring(1).toInt(), 0, time);
+    }
+    else{
+        Serial.println("Invalid command");
+    }
+}
+
+
