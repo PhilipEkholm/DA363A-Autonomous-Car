426e4186c43c84ed776436856bd9579c3284acda
Philip Ekholm <philipekholm@protonmail.com>
2017-05-15 15:34:04 +0200

    Restored rc_driver

--- computer/rc_driver.py
+++ computer/rc_driver.py
@@ -10,6 +10,7 @@ import math
 # distance data measured by ultrasonic sensor
 sensor_data = " "
 
+
 class NeuralNetwork(object):
 
     def __init__(self):
@@ -28,7 +29,7 @@ class NeuralNetwork(object):
 class RCControl(object):
 
     def __init__(self):
-        self.serial_port = serial.Serial('/dev/tty.usbmodem1411', 115200, timeout=1)
+        self.serial_port = serial.Serial('/dev/tty.usbmodem1421', 115200, timeout=1)
 
     def steer(self, prediction):
         if prediction == 2:
@@ -43,23 +44,6 @@ class RCControl(object):
         else:
             self.stop()
 
-    def override(self, command):
-        command = '4'
-        if command == '1':
-            print ('Override: Forward')
-            self.serial_port.write(command)
-        elif command == '2':
-            print ('Override: Reverse')
-            self.serial_port.write(command)
-        elif command == '3':
-            print ('Override: Right')
-            self.serial_port.write(command)
-        elif command == '4':
-            print ('Override: Left')
-            self.serial_port.write(command)
-        else:
-            print 'Invalid command: ' + command
-
     def stop(self):
         self.serial_port.write(chr(0))
 
@@ -68,7 +52,7 @@ class DistanceToCamera(object):
 
     def __init__(self):
         # camera params
-        self.alpha = 10.0 * math.pi / 180
+        self.alpha = 8.0 * math.pi / 180
         self.v0 = 119.865631204
         self.ay = 332.262498472
 
@@ -114,6 +98,7 @@ class ObjectDetection(object):
             # stop sign
             if width/height == 1:
                 cv2.putText(image, 'STOP', (x_pos, y_pos-10), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
+            
             # traffic lights
             else:
                 roi = gray_image[y_pos+10:y_pos + height-10, x_pos+10:x_pos + width-10]
@@ -140,19 +125,6 @@ class ObjectDetection(object):
                     #    self.yellow_light = True
         return v
 
-class ClientOverrideHandler(SocketServer.BaseRequestHandler):
-    car = RCControl()
-
-    def handle(self):
-        try:
-            while True:
-                self.data = self.request.recv(1024).strip()
-                
-                if self.data != '':
-                    self.car.override(self.data)
-        finally:
-            print 'Connection closed on thread 3'
-
 
 class SensorDataHandler(SocketServer.BaseRequestHandler):
 
@@ -162,8 +134,7 @@ class SensorDataHandler(SocketServer.BaseRequestHandler):
         global sensor_data
         try:
             while self.data:
-                #Only get the latest value and trim anything else off
-                self.data = self.request.recv(1024)[0:10]
+                self.data = self.request.recv(1024)
                 sensor_data = round(float(self.data), 1)
                 #print "{} sent:".format(self.client_address[0])
                 print sensor_data
@@ -177,8 +148,6 @@ class VideoStreamHandler(SocketServer.StreamRequestHandler):
     h1 = 15.5 - 10  # cm
     # h2: traffic light
     h2 = 15.5 - 10
-    # h3: direction sign
-    h3 = 15.5 - 10
 
     # create neural network
     model = NeuralNetwork()
@@ -190,12 +159,10 @@ class VideoStreamHandler(SocketServer.StreamRequestHandler):
     # cascade classifiers
     stop_cascade = cv2.CascadeClassifier('cascade_xml/stop_sign.xml')
     light_cascade = cv2.CascadeClassifier('cascade_xml/traffic_light.xml')
-    direction_cascade = cv2.CascadeClassifier('cascade_xml/danial.xml')
 
     d_to_camera = DistanceToCamera()
     d_stop_sign = 25
     d_light = 25
-    d_direction_sign = 25
 
     stop_start = 0              # start time when stop at the stop sign
     stop_finish = 0
@@ -208,7 +175,6 @@ class VideoStreamHandler(SocketServer.StreamRequestHandler):
         stream_bytes = ' '
         stop_flag = False
         stop_sign_active = True
-        directionFlag = False
 
         # stream video frames one by one
         try:
@@ -228,14 +194,11 @@ class VideoStreamHandler(SocketServer.StreamRequestHandler):
                     # object detection
                     v_param1 = self.obj_detection.detect(self.stop_cascade, gray, image)
                     v_param2 = self.obj_detection.detect(self.light_cascade, gray, image)
-                    v_param3 = self.obj_detection.detect(self.direction_cascade, gray, image)
 
                     # distance measurement
                     if v_param1 > 0 or v_param2 > 0:
                         d1 = self.d_to_camera.calculate(v_param1, self.h1, 300, image)
                         d2 = self.d_to_camera.calculate(v_param2, self.h2, 100, image)
-                        d3 = self.d_to_camera.calculate(v_param3, self.h3, 300, image)
-                        self.d_direction_sign = d3
                         self.d_stop_sign = d1
                         self.d_light = d2
 
@@ -249,7 +212,7 @@ class VideoStreamHandler(SocketServer.StreamRequestHandler):
                     prediction = self.model.predict(image_array)
 
                     # stop conditions
-                    if sensor_data is not None and sensor_data < 30 and sensor_data > 5:
+                    if sensor_data is not None and sensor_data < 30:
                         print("Stop, obstacle in front")
                         self.rc_car.stop()
                     
@@ -288,8 +251,6 @@ class VideoStreamHandler(SocketServer.StreamRequestHandler):
                         self.obj_detection.red_light = False
                         self.obj_detection.green_light = False
                         self.obj_detection.yellow_light = False
-                    elif 0 < self.d_direction_sign < 25:
-                        print('Direction Sign ahead')
 
                     else:
                         self.rc_car.steer(prediction)
@@ -312,6 +273,7 @@ class VideoStreamHandler(SocketServer.StreamRequestHandler):
 
 
 class ThreadServer(object):
+
     def server_thread(host, port):
         server = SocketServer.TCPServer((host, port), VideoStreamHandler)
         server.serve_forever()
@@ -320,16 +282,10 @@ class ThreadServer(object):
         server = SocketServer.TCPServer((host, port), SensorDataHandler)
         server.serve_forever()
 
-    def server_thread3(host, port):
-        server = SocketServer.TCPServer((host, port), ClientOverrideHandler)
-        server.serve_forever()
-
-    distance_thread = threading.Thread(target=server_thread2, args=('192.168.20.175', 8002))
+    distance_thread = threading.Thread(target=server_thread2, args=('192.168.1.100', 8002))
     distance_thread.start()
-    video_thread = threading.Thread(target=server_thread, args=('192.168.20.175', 8000))
+    video_thread = threading.Thread(target=server_thread('192.168.1.100', 8000))
     video_thread.start()
-    #client_thread = threading.Thread(target=server_thread3('192.168.20.175', 8003))
-    #client_thread.start()
 
 if __name__ == '__main__':
     ThreadServer()
