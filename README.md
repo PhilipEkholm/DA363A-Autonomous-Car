8dc12c3ffd855bcbd1d390845a617eca71dda77a
Philip Ekholm <philipekholm@protonmail.com>
2017-05-30 12:48:41 +0200

    Commented CarController

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
 # P1-Autonomous-Car
 Welcome to proably the most exciting project amongst engineers in the year of 2017! Readme.md files can be found pretty much in most folders and will explain the overall function of the code. Be sure to follow the readme.md files to understand which files are important to follow!
+
+Interesting code:
+	- AndroidClient/
+		app/
+			src/
+
+	- arduino-embedded/
+		CarController/
+
+	- raspberrypi-embedded/
+		* AndroidComms.py
+		* AutoCommunication.py
+		* stream_video.py
+		* ultrasonic_client.py
+
+	- server/
+		* collect_train_data_socket.py
+		* mlp_predict_test.py
+		* mlp_training.py
+		* picam_calibration.py
+		* rc_driver.py
--- arduino-embedded/CarController/CarController.ino
+++ arduino-embedded/CarController/CarController.ino
@@ -18,13 +18,19 @@ const byte DIR1_PINA = 5;  //Backwards pin
 const byte DIR2_PINA = 3; //Forwards pin 
 const byte SPEED_PINA = 11; // Needs to be a PWM pin to be able to control motor speed 
 const byte MOTOR_SPEED = 195; //Change motor speed 
-//50 
+
+//Middle point of the servo
 const byte MIDDLE_POINT_SERVO = 50;
 int command = 0; 
 int lastCommand = 0; 
 int time = 50; 
 
+/**
+ *  Setup function which is called first in the program, used for configuration
+ */
+
 void setup(){ 
+    //Serial runs at 115 200 Baud
 	Serial.begin(115200);  
 	 
 	//Define L298N Dual H-Bridge Motor Controller Pins 
@@ -34,9 +40,15 @@ void setup(){
 	pinMode(SPEED_PINA,OUTPUT); 
 	pointer.attach(SERVO_PIN); 
 
+    //Set default values (stop motor and keep wheels straight)
 	motor_run(0, 1, 10); 
 	steer(MIDDLE_POINT_SERVO, 10); 
 } 
+
+/**
+ * Loop forever while the Arduino has power, will constantly listen
+ * to serial-port for inputs, interprets command as well as stores it.
+ */
  
 void loop(){ 
     if (Serial.available() > 0){ 
@@ -47,16 +59,23 @@ void loop(){
     lastCommand = command; 
 } 
 
+/**
+ *  Set car at its default state if command 0 is passed
+ */
 void reset(){ 
 	motor_run(0, 0, 50); 
 
 	steer(MIDDLE_POINT_SERVO, 50);
 } 
  
-/* 
-*   The golden method! Control the motor with 
-*   a certain speed (0 to 100). Direction: 0 reverse, 1 forward. 
-*/ 
+/**
+ *  Controls the speed of the motor as well as the direction of the
+ *  DC-motor.
+ *   
+ * @param   speed the speed of the motor to keep which is a value between 0 (stop) and 255 (full power)
+ * @param   direction direction of the motor, where 1 means forward and 0 is backwards
+ * @param   time embedded delay of operation, usually 50 milliseconds
+ */
 
 void motor_run(int speed, int direction, int time){ 
     if(direction == 1){ 
@@ -72,6 +91,12 @@ void motor_run(int speed, int direction, int time){
  
     delay(time); 
 } 
+
+/**
+ * @param   capacity capacity is the direction to steer from 0 to 100, where 0 is to
+ *          the right viewed from the forward direction of the car.
+ * @param   time embedded delay of operation, usually 50 milliseconds.
+ */
 
 void steer(int capacity, int time){ 
     if(capacity > 100 || capacity < 0){ 
@@ -92,40 +117,53 @@ void steer(int capacity, int time){
     delay(time); 
 } 
 
+/**
+ * @param   command the command that was passed through serial-port
+ * @param   time embedded delay of operation, usually 50 milliseconds
+ */
+
 void send_command(int command, int time){ 
   switch (command){ 
  
      //reset command 
      case 0: reset(); break; 
  
-     // single command 
+     // simple command 
      case 1: 
-     	motor_run(MOTOR_SPEED, 1, time);  
+        //Forward
+     	motor_run(MOTOR_SPEED, 1, time);
         break; 
      case 2: 
+        //Backward
      	motor_run(MOTOR_SPEED, 0, time); 
         break; 
      case 3: 
+        //Right
      	steer(0, time);  
         break; 
      case 4: 
+        //Left
      	steer(100, time);  
         break; 
  
-     //combination command 
+     //complex command 
      case 6: 
+        //Forward right
      	motor_run(MOTOR_SPEED, 1, time); 
         steer(0, time); 
         break; 
      case 7: 
+        //Forward left
      	motor_run(MOTOR_SPEED, 1, time); 
         steer(100,time); 
         break; 
      case 8: 
+        //Backward right
      	steer(0,time); 
         motor_run(MOTOR_SPEED, 0, time); 
         break; 
      case 9: 
+        //Backward left
      	steer(100,time);
         motor_run(MOTOR_SPEED, 0, time);
         break; 
